dcffdce7c4350e21372f44c89d2133456eefc8d3
dg <dg>
2013-08-17 19:30:24 +0000

    Initial checkin.

---
+++ lib/com/cowlark/luje/Main.java
@@ -0,0 +1,16 @@
+package com.cowlark.luje;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: dg
+ * Date: 17/08/13
+ * Time: 20:15
+ * To change this template use File | Settings | File Templates.
+ */
+public class Main
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Hello, world!");
+	}
+}
---
+++ vm/main.lua
@@ -0,0 +1,10 @@
+--
+-- Created by IntelliJ IDEA.
+-- User: dg
+-- Date: 17/08/13
+-- Time: 20:16
+-- To change this template use File | Settings | File Templates.
+--
+
+print("Hello, world!")
+
