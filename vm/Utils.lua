ec0747684147b5d78efcb6f1bc4ac1353f4d9308
dg <dg>
2013-10-06 17:16:40 +0000

    Update copyright notices.

--- vm/Utils.lua
+++ vm/Utils.lua
@@ -1,3 +1,9 @@
+-- Luje
+-- © 2013 David Given
+-- This file is redistributable under the terms of the
+-- New BSD License. Please see the COPYING file in the
+-- project root for the full text.
+
 local ServerDir = ServerDir
 
 --- Halts with a user error (no stack trace).
