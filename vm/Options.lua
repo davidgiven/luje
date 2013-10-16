1ef5ab6c9d8145a727ba98a610ff4d4d05f6115a
dg <dg>
2013-10-16 11:08:41 +0000

    Add missing file!

---
+++ vm/Options.lua
@@ -0,0 +1,12 @@
+-- Luje
+-- © 2013 David Given
+-- This file is redistributable under the terms of the
+-- New BSD License. Please see the COPYING file in the
+-- project root for the full text.
+
+return {
+	CheckNullPointers = true,
+	DumpCompilations = false,
+	TraceCompilations = false
+}
+
